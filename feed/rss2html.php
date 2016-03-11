dcf4e3c6b3969a3bcb4d4b63334a42a75f358301
Sue Feng <sue@suefeng.com>
2016-03-11 16:36:37 -0600

    initial commit

---
+++ feed/rss2html.php
@@ -0,0 +1,6 @@
+        <span class="title"><a href=\"$href\">~~~FeedTitle~~~</a></span>
+		~~~BeginItemsRecord~~~
+		<strong>~~~ItemPubShortDate~~~ ~~~ItemPubShortTime~~~</strong><br />
+		<a href="~~~ItemLink~~~">~~~ItemTitle~~~</a><br />
+		<div class="description">~~~ItemDescription~~~</div>
+		~~~EndItemsRecord~~~
